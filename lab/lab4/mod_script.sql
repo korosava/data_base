d3ebed9e4ff23ec88fa3ca7b41157508fff9a651
yura <yuriytom81@gmail.com>
2019-04-08 23:44:25 +0300

    lab4_changes

---
+++ lab/lab4/mod_script.sql
@@ -0,0 +1,5 @@
+insert into zoo.roles values (1, 'cashier', 1500);
+insert into zoo.roles values (null, 'breadwinner', 1700),
+							 (null, 'vet', '2500'),
+                             (null, 'cleaner', '1500');
+                             
